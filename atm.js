432fc717e4857d4a126a5bc9244734cad5cfdfe3
Carryl Sorene <carrylsorene@gmail.com>
2015-08-19 18:32:39 +0100

    Each part pseudocoded and begun

---
+++ atm.js
@@ -0,0 +1,72 @@
+   $(document).ready(function(){
+  
+})
+
+var currentBalance = $('balance1') === 0;
+var savingsBalance = $('balance2') === 0;
+
+//Deposit amount, --calculate -- update variable, display new amount and update with future deposits too
+
+//Current Account
+currentBalance = $('amount1')..value ////?order //event is change?
+
+//current
+eL for click on button $('deposit')
+ditto $('withdraw')
+
+//savings
+eL for click on button $('deposit')
+ditto $('withdraw')
+
+if (amount1 === Nan) {
+  return "Please enter valid numbers"; //parseFloat? 
+}
+
+$('balance1').html = currentBalance //function?
+//return text to box - 
+//next time, add to var, not just change it
+
+if (currentBalance === 0) {
+  $('balance1')css('background', 'red');
+}
+//Savings Account
+
+$('amount2') //ditto
+
+$('balance2').html = savingsBalance
+
+//Withdraw amount, update variable, display new amount and update with future withdrawals too 
+
+savingsBalance = $('amount1').change.value  
+
+if (amount1 !== //number) {
+  return "Please enter valid numbers";
+}
+
+$('balance2').html = savingsBalance //function?
+
+//change bgcolor to red if balance is 0
+if (currentBalance === 0) {
+  $('balance1')css('background', 'red');
+}
+
+//if too much taken
+
+//try to take too much from current
+ $('amount2').change.value
+ if (amount1 > balance1) && (amount1 > balance2) {
+  balance1 - amount1 === balance1 // ?logic think
+  balance2 - amount1 === balance2 // ?ditto
+}
+
+//try to take too much from savings
+ $('amount2').change.value
+ if (amount1 > balance1) && (amount1 > balance2) {
+    return .html('The balance in both account is too low.')
+ }
+
+
+
+
+
+
