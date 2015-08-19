937cf87615dd590857500ca468e799ef5bfb6199
Carryl Sorene <carrylsorene@gmail.com>
2015-08-19 20:49:07 +0100

    Deposit working, withdrawal nearly, the rest there in theory not tested yet

--- atm.js
+++ atm.js
@@ -1,72 +1,86 @@
-   $(document).ready(function(){
-  
-})
+$(document).ready(function(){
 
-var currentBalance = $('balance1') === 0;
-var savingsBalance = $('balance2') === 0;
+var currentStartBalance = 0;
+console.log('first boo')
+var savingsStartBalance = 0;
+
+var savingsNewBalance = 0;
+var currentNewBalance = 0;
 
 //Deposit amount, --calculate -- update variable, display new amount and update with future deposits too
 
-//Current Account
-currentBalance = $('amount1')..value ////?order //event is change?
+//Current Account -- deposit
 
-//current
-eL for click on button $('deposit')
-ditto $('withdraw')
-
-//savings
-eL for click on button $('deposit')
-ditto $('withdraw')
-
-if (amount1 === Nan) {
-  return "Please enter valid numbers"; //parseFloat? 
-}
-
-$('balance1').html = currentBalance //function?
-//return text to box - 
-//next time, add to var, not just change it
-
-if (currentBalance === 0) {
-  $('balance1')css('background', 'red');
-}
-//Savings Account
-
-$('amount2') //ditto
-
-$('balance2').html = savingsBalance
-
-//Withdraw amount, update variable, display new amount and update with future withdrawals too 
-
-savingsBalance = $('amount1').change.value  
-
-if (amount1 !== //number) {
-  return "Please enter valid numbers";
-}
-
-$('balance2').html = savingsBalance //function?
-
-//change bgcolor to red if balance is 0
-if (currentBalance === 0) {
-  $('balance1')css('background', 'red');
-}
-
-//if too much taken
-
-//try to take too much from current
- $('amount2').change.value
- if (amount1 > balance1) && (amount1 > balance2) {
-  balance1 - amount1 === balance1 // ?logic think
-  balance2 - amount1 === balance2 // ?ditto
-}
-
-//try to take too much from savings
- $('amount2').change.value
- if (amount1 > balance1) && (amount1 > balance2) {
-    return .html('The balance in both account is too low.')
- }
+ $('#deposit1').on('click', function(e){ 
+    currentNewBalance = parseInt($('#amount1').val());
+    console.log(currentNewBalance)
+    $('#balance1').text(currentNewBalance);
+    currentStartBalance = currentStartBalance + currentNewBalance;
+    $('#balance1').text('$' + currentStartBalance);
+    console.log(currentNewBalance);
+    console.log("You've got cash!"); 
+  });
 
 
+//Savings Account -- deposit
+
+ $('#deposit2').on('click', function(e){ 
+    savingsNewBalance = parseInt($('#amount2').val());
+    console.log(savingsNewBalance)
+    $('#balance2').text(savingsNewBalance);
+    savingsStartBalance = savingsStartBalance + savingsNewBalance;
+    $('#balance2').text('$' + savingsStartBalance);
+    console.log(savingsNewBalance);
+    console.log("You're socking it away!");
+  });
+
+//Current Account -- withdraw
+
+$('#withdraw1').on('click', function(e){ 
+    currentNewBalance = parseInt($('#amount1').val());
+    console.log(currentNewBalance)
+    $('#balance1').text(currentNewBalance);
+    currentStartBalance = currentNewBalance - currentStartBalance;
+    $('#balance1').text('$' + currentStartBalance);
+    console.log(currentNewBalance);
+    console.log("Buy something nice!"); 
+  });
 
 
+// //Savings Account -- withdraw
 
+$('#withdraw2').on('click', function(e){ 
+    currentNewBalance = parseInt($('#amount2').val());
+    console.log(currentNewBalance)
+    $('#balance2').text(currentNewBalance);
+    currentStartBalance = currentNewBalance - ;
+    $('#balance2').text('$' + currentStartBalance);
+    console.log(currentNewBalance);
+    console.log("Keep some for later!"); 
+  });
 
+//   if (#amount1 === Nan) || if (#amount2 === NaN) {
+//     $ "Please enter valid numbers"; //print to the right class or id
+//     };
+
+//   if (currentNewBalance === 0) {
+//     $('#balance1').css('background', 'red');
+//   };
+
+//   if (savingsNewBalance === 0) {
+//     $('#balance2').css('background', 'red');
+//   };
+
+// //try to take too much from current
+
+//   if ($('amount1').click.val() > currentNewBalance) && ($('amount1').click.val() > savingsNewBalance) {
+//       (savingsNewBalance = savingsNewBalance - amount1) && (currentNewBalance = currentNewBalance - amount1) // ?&& //unless there's not enough
+//   };
+
+// //try to take too much from savings
+
+//   if ($('amount1').click.val() > currentNewBalance) && ($('amount1').click.val() > savingsNewBalance) {
+//     $('.clear', 'The balance in both account is too low.')
+//   };
+
+ })
